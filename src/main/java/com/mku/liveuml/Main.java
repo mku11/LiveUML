97c908c60e9db263f044f2289f4d5c2c18f359f6
mku11 <93253421+mku11@users.noreply.github.com>
2024-11-15 21:30:23 +0200

    fixed name

--- src/main/java/com/mku/liveuml/Main.java
+++ src/main/java/com/mku/liveuml/Main.java
@@ -60,7 +60,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setTitle("Live UML");
+        f.setTitle("LiveUML");
         f.setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/icons/logo.png")));
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
